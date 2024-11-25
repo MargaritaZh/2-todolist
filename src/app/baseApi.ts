21b48f3ad0472c1cff5feddb56b4ea54f56e0962
MargaritaZh <margaritazhitkevich@gmail.com>
2024-11-25 21:36:43 +0300

    lesson 19 RTK query. Create handleError

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,9 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { setAppError } from "./appSlice"
-import { Simulate } from "react-dom/test-utils"
-import error = Simulate.error
-import { ResultCode } from "common/enums"
-//обязательно в импорте в конце библиотека react
+import { handleError } from "common/utils/handleError"
+
+//обязательно в импорте createApi в конце библиотека react
 
 // export const baseApi = createApi({
 //   reducerPath: "todolistsApi",
@@ -32,38 +30,8 @@ export const baseApi = createApi({
       }
     })(args, api, extraOptions)
 
-    //1 Global Query errors
-    let error = 'Some error occurred'
-
-    if (result.error) {
-      switch (result.error.status) {
-        case 'FETCH_ERROR':
-        case 'PARSING_ERROR':
-        case 'CUSTOM_ERROR':
-          error = result.error.error
-          break
-        case 403:
-          error = '403 Forbidden Error. Check API-KEY'
-          break
-        case 401:
-          error = '401 Error. Check token'
-          break
-        case 400:
-          error = (result.error.data as { message: string }).message
-          break
-        default:
-          error = JSON.stringify(result.error)
-          break
-      }
-      api.dispatch(setAppError({ error }))
-    }
-
-    // 2. Result code errors
-    if ((result.data as { resultCode: ResultCode }).resultCode === ResultCode.Error) {
-      const messages = (result.data as { messages: string[] }).messages
-      error = messages.length ? messages[0] : error
-      api.dispatch(setAppError({ error }))
-    }
+    //обработка ошибок
+    handleError(api, result)
 
     return result
   },
